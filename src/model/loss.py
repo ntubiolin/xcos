e0e8f280b9ebbecd5f5c80d991000e0f038228d0
YaLiangChang <b03901014@ntu.edu.tw>
2019-07-09 20:35:42 +0800

    Clean model folder to MNIST model

--- src/model/loss.py
+++ src/model/loss.py
@@ -1,6 +1,4 @@
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 
 class CrossEntropyLoss(nn.Module):
@@ -14,18 +12,3 @@ class CrossEntropyLoss(nn.Module):
         logits = output[self.output_key]
         target = data[self.target_key]
         return self.loss_fn(logits, target)
-
-
-class FocalLoss(nn.Module):
-    def __init__(self, output_key='verb_logits', target_key='verb_class', gamma=2, weight=None):
-        super(FocalLoss, self).__init__()
-        self.output_key = output_key
-        self.target_key = target_key
-        self.gamma = gamma
-        self.nll_loss = torch.nn.NLLLoss(weight)
-
-    def forward(self, data, output):
-        logits = output[self.output_key]
-        target = data[self.target_key]
-
-        return self.nll_loss((1 - F.softmax(logits, 1)) ** self.gamma * F.log_softmax(logits, 1), target)
