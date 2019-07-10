f49da69c4738d9264b689f107d6886d5c1bcd9fc
YaLiangChang <b03901014@ntu.edu.tw>
2019-07-10 17:56:03 +0800

    Add MNIST dataset and dataloader

---
+++ src/data_loader/mnist.py
@@ -0,0 +1,17 @@
+from torchvision import datasets
+
+
+class MnistDataset(datasets.MNIST):
+    """
+    Customized MNIST dataset demo
+    """
+    def __init__(self, data_dir, train, download, transform):
+        super().__init__(data_dir, train=train, download=download, transform=transform)
+
+    def __getitem__(self, index):
+        """ Overwrite __getitem__ to return dictionary """
+        data = super().__getitem__(index)
+        return {
+            "data_input": data[0],
+            "data_target": data[1]
+        }
