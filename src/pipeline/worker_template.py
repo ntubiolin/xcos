9f3c44bc5672ba5d39ca2adf8f76f9d25b1036bc
YaLiangChang <b03901014@ntu.edu.tw>
2019-07-12 13:31:53 +0800

    Recover scalar writing when getting loss/metrics

--- src/pipeline/worker_template.py
+++ src/pipeline/worker_template.py
@@ -24,32 +24,34 @@ class WorkerTemplate:
     def _setup_writer(self):
         self.writer.set_step(self.step, self.data_loader.name)
 
-    def _get_weighted_losses(self, data, model_output):
-        weighted_losses = {}
+    def _get_and_write_loss(self, data, model_output):
+        losses = []
         for loss_name, (loss_instance, loss_weight) in self.losses.items():
             if loss_weight <= 0.0:
                 continue
-            weighted_loss = loss_instance(data, model_output) * loss_weight
-            weighted_losses[loss_name] = weighted_loss
-        return weighted_losses
+            loss = loss_instance(data, model_output) * loss_weight
+            losses.append(loss)
+            self.writer.add_scalar(f'{loss_name}', loss.item())
+        loss = sum(losses)
+        self.writer.add_scalar('loss', loss.item())
+        return loss
 
-    def _eval_metrics(self, data_input, model_output):
+    def _get_and_write_metrics(self, data, model_output):
         acc_metrics = np.zeros(len(self.metrics))
         for i, metric in enumerate(self.metrics):
-            acc_metrics[i] += metric(data_input, model_output)
+            acc_metrics[i] += metric(data, model_output)
             self.writer.add_scalar(f'{metric.__name__}', acc_metrics[i])
         return acc_metrics
 
     def _run_and_optimize_model(self, data):
         self.optimizer.zero_grad()
         model_output = self.model(data)
-        weighted_losses = self._get_weighted_losses(data, model_output)
-        loss = sum(weighted_losses.values())
+        loss = self._get_and_write_loss(data, model_output)
         loss.backward()
         self.optimizer.step()
 
-        metrics = self._eval_metrics(data, model_output)
-        return model_output, loss, weighted_losses, metrics
+        metrics = self._get_and_write_metrics(data, model_output)
+        return model_output, loss, metrics
 
     def _data_to_device(self, data):
         for key in data.keys():
@@ -57,11 +59,6 @@ class WorkerTemplate:
             if torch.is_tensor(data[key]):
                     data[key] = data[key].to(self.device)
         return data
-
-    def _write_scalers(self, loss, weighted_losses, metrics):
-        for loss_name, weighted_loss in weighted_losses.items():
-            self.writer.add_scalar(f'weighted_{loss_name}', weighted_loss.item())
-        self.writer.add_scalar('loss', loss.item())
 
     def _write_images(self, data, model_output):
         self.writer.add_image("data_input", make_grid(data["data_input"], nrow=4, normalize=True))
@@ -77,9 +74,8 @@ class WorkerTemplate:
 
             data = self._data_to_device(data)
 
-            model_output, loss, weighted_losses, metrics = self._run_and_optimize_model(data)
+            model_output, loss, metrics = self._run_and_optimize_model(data)
 
-            self._write_scalers(loss, weighted_losses, metrics)
             if batch_idx % self.log_step == 0:
                 self._write_images(data, model_output)
                 if self.verbosity >= 2:
@@ -91,13 +87,24 @@ class WorkerTemplate:
         epoch_time = time.time() - epoch_start_time
         avg_loss = total_loss / len(self.data_loader)
         avg_metrics = (total_metrics / len(self.data_loader)).tolist()
-        self._record_log(epoch_time, avg_loss, avg_metrics)
+        return epoch_time, avg_loss, avg_metrics
+
+    def _to_log(self, epoch, epoch_time, avg_loss, avg_metrics):
+        log = {
+            'epoch': epoch,
+            'epoch_time': epoch_time,
+            'avg_loss': avg_loss,
+            'avg_metrics': avg_metrics
+        }
+        return log
 
     def _setup_lr_scheduler(self):
         if self.lr_scheduler is not None:
             self.lr_scheduler.step()
 
-    def run(self):
+    def run(self, epoch):
         self._setup_model()
-        self._iter_data()
+        epoch_time, avg_loss, avg_metrics = self._iter_data()
         self._setup_lr_scheduler()
+        log = self._to_log(epoch, epoch_time, avg_loss, avg_metrics)
+        return log
