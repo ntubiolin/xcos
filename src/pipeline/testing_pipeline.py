6e9e7bd6077fe8d14804ff1909b9c31a19e22a52
YaLiangChang <b03901014@ntu.edu.tw>
2019-07-12 15:42:00 +0800

    Add tester.py

--- src/pipeline/testing_pipeline.py
+++ src/pipeline/testing_pipeline.py
@@ -1,36 +1,16 @@
-import math
-
 from .base_pipeline import BasePipeline
-from .trainer import Trainer
-from .validator import Validator
+from .tester import Tester
 
 
 class TestingPipeline(BasePipeline):
 
-    def _setup(self):
-        self.epochs = self.config['trainer']['epochs']
-        self.save_freq = self.config['trainer']['save_freq']
-        self.verbosity = self.config['trainer']['verbosity']
-
-        # configuration to monitor model performance and save best
-        self.monitor = self.config['trainer']['monitor']
-        self.monitor_mode = self.config['trainer']['monitor_mode']
-        assert self.monitor_mode in ['min', 'max', 'off']
-        self.monitor_best = math.inf if self.monitor_mode == 'min' else -math.inf
-
-        self.do_validation = len(self.valid_data_loaders) > 0
-
-        self.start_epoch = 1
-        self.train_iteration_count = 0
-        self.valid_iteration_counts = [0 for _ in range(len(self.valid_data_loaders))]
+    def _setup_config(self):
+        pass
 
     def _create_workers(self):
-        trainer = Trainer(
+        tester = Tester(
             self.config, self.model, self.data_loader,
             self.losses, self.metrics, self.optimizer, self.writer
         )
-        validator = Validator(
-            self.config, self.model, self.data_loader,
-            self.losses, self.metrics, self.optimizer, self.writer
-        )
-        self.workers = [trainer, validator]
+        workers = [tester]
+        return workers
