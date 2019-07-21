7f5b4ace686b352d4fa7759abd253073cc0d3dd0
YaLiangChang <b03901014@ntu.edu.tw>
2019-07-21 17:04:46 +0800

    Let SingleGlobalConfig inherits AttriDict

--- src/utils/global_config.py
+++ src/utils/global_config.py
@@ -1,32 +1,41 @@
 import json
 
-from logging_config import logger
+from attrdict import AttrDict
+
+from .logging_config import logger
 
 
-class SingleGlobalConfig:
-    def __init__(self):
-        self.config = {}
+class SingleGlobalConfig(AttrDict):
+    """ The global config object for all module configuration.
+
+    It needs to be setup first by main.py
+
+    One could use either global_config.some_attribute or global_config['some_attribute']
+    to access the config
+    """
 
     def set_config(self, config):
-        self.config = config
+        for k, v in config:
+            self[k] = v
 
     def extend_configs(self, config_filenames: list):
         # load config files, the overlapped entries will be overwriten
         for config_filename in config_filenames:
             with open(config_filename) as fin:
                 config = json.load(fin)
-                self.extend_config(config)
+                self._extend_config(config)
 
-    def extend_config(self, added_config):
+    def _extend_config(self, added_config):
         for key, value in added_config.items():
-            if key in self.config.keys():
+            if key in self.keys():
                 if key == 'name':
-                    value = f"{self.config[key]}_{value}"
+                    value = f"{self[key]}_{value}"
                 else:
                     logger.warning(f"Overriding '{key}' in config")
-                del self.config[key]
-            self.config[key] = value
-        return self.config
+                del self[key]
+            self[key] = value
 
 
+# Initialize this global_config first
+# and then for all modules, import this config
 global_config = SingleGlobalConfig()
