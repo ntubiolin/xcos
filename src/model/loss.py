eb595918d2ef8e549464e964901ec93aa56da720
ZheYu <j2325138@gmail.com>
2019-07-21 15:34:44 +0800

    Refactor loss: create BaseLoss for common behavior of losses.

--- src/model/loss.py
+++ src/model/loss.py
@@ -1,16 +1,29 @@
 import torch.nn as nn
 
 
-class CrossEntropyLoss(nn.Module):
+class BaseLoss(nn.Module):
     def __init__(self, output_key, target_key, nickname=None, weight=1):
-        super(CrossEntropyLoss, self).__init__()
-        self.loss_fn = nn.CrossEntropyLoss()
+        super().__init__()
         self.output_key = output_key
         self.target_key = target_key
         self.weight = weight
         self.nickname = self.__class__.__name__ if nickname is None else nickname
 
-    def forward(self, data, output):
-        logits = output[self.output_key]
-        target = data[self.target_key]
-        return self.loss_fn(logits, target)
+    def _preproces(self, data_dict, output_dict):
+        return data_dict, output_dict
+
+    def _postprocess(self, output, target):
+        return output, target
+
+    def forward(self, data_dict, output_dict):
+        data_dict, output_dict = self._preproces(data_dict, output_dict)
+        output = output_dict[self.output_key]
+        target = data_dict[self.target_key]
+        output, target = self._postprocess(output, target)
+        return self.loss_fn(output, target)
+
+
+class CrossEntropyLoss(BaseLoss):
+    def __init__(self, *args, **kargs):
+        super().__init__(*args, **kargs)
+        self.loss_fn = nn.CrossEntropyLoss()
