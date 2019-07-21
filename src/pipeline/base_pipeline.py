e8c3ccc038a716758905214f8829c778b545bf94
YaLiangChang <b03901014@ntu.edu.tw>
2019-07-21 16:32:53 +0800

    Replace self.config in base_pipeline with global_config

--- src/pipeline/base_pipeline.py
+++ src/pipeline/base_pipeline.py
@@ -8,6 +8,7 @@ import torch
 from utils.util import ensure_dir, get_instance
 from utils.visualization import WriterTensorboardX
 from utils.logging_config import logger
+from utils.global_config import global_config
 import data_loader.data_loaders as module_data
 import model.metric as module_metric
 import model.model as module_arch
@@ -18,9 +19,8 @@ class BasePipeline(ABC):
     Base pipeline for training/validation/testing process
     """
     def __init__(
-        self, args, config
+        self, args
     ):
-        self.config = config
         self.start_time = datetime.datetime.now().strftime('%m%d_%H%M%S')
         self._setup_device()
         self._setup_data_loader()
@@ -33,7 +33,6 @@ class BasePipeline(ABC):
         self._setup_evaluation_metrics()
 
         self._setup_config()
-        self.verbosity = config['trainer']['verbosity']
 
         if args.resume is not None:
             self._resume_checkpoint(args.resume)
@@ -69,14 +68,14 @@ class BasePipeline(ABC):
             device = torch.device('cuda:0' if n_gpu_use > 0 else 'cpu')
             list_ids = list(range(n_gpu_use))
             return device, list_ids
-        self.device, self.device_ids = prepare_device(self.config['n_gpu'])
+        self.device, self.device_ids = prepare_device(global_config['n_gpu'])
 
     def _setup_model_and_optimizer(self):
         """ Setup model and optimizer
 
         Load pretrained / resume checkpoint / data parallel if specified """
         model = get_instance(
-            module_arch, 'arch', self.config,
+            module_arch, 'arch', global_config,
         )
         # Print out the model architecture and number of parameters
         model.summary()
@@ -88,13 +87,13 @@ class BasePipeline(ABC):
             self.model = torch.nn.DataParallel(model, device_ids=self.device_ids)
 
     def _setup_data_loader(self):
-        self.data_loader = get_instance(module_data, 'data_loader', self.config)
+        self.data_loader = get_instance(module_data, 'data_loader', global_config)
 
     def _setup_valid_data_loaders(self):
-        if 'valid_data_loaders' in self.config.keys():
+        if 'valid_data_loaders' in global_config.keys():
             self.valid_data_loaders = [
                 getattr(module_data, entry['type'])(**entry['args'])
-                for entry in self.config['valid_data_loaders']
+                for entry in global_config['valid_data_loaders']
             ]
         elif self.data_loader.validation_split > 0:
             self.valid_data_loaders = [self.data_loader.split_validation()]
@@ -104,26 +103,26 @@ class BasePipeline(ABC):
     def _setup_evaluation_metrics(self):
         self.evaluation_metrics = [
             getattr(module_metric, entry['type'])(**entry['args'])
-            for entry in self.config['metrics']
+            for entry in global_config['metrics']
         ]
 
     def _setup_optimizer(self):
         trainable_params = filter(lambda p: p.requires_grad, self.model.parameters())
-        self.optimizer = get_instance(torch.optim, 'optimizer', self.config, trainable_params)
+        self.optimizer = get_instance(torch.optim, 'optimizer', global_config, trainable_params)
 
     def _setup_checkpoint_dir(self):
-        self.checkpoint_dir = os.path.join(self.config['trainer']['save_dir'], self.config['name'], self.start_time)
+        self.checkpoint_dir = os.path.join(global_config['trainer']['save_dir'], global_config['name'], self.start_time)
         # Save configuration file into checkpoint directory:
 
         ensure_dir(self.checkpoint_dir)
         config_save_path = os.path.join(self.checkpoint_dir, 'config.json')
         with open(config_save_path, 'w') as handle:
-            json.dump(self.config, handle, indent=4, sort_keys=False)
+            json.dump(global_config, handle, indent=4, sort_keys=False)
 
     def _setup_writer(self):
         # setup visualization writer instance
-        writer_dir = os.path.join(self.config['visualization']['log_dir'], self.config['name'], self.start_time)
-        self.writer = WriterTensorboardX(writer_dir, logger, self.config['visualization']['tensorboardX'])
+        writer_dir = os.path.join(global_config['visualization']['log_dir'], global_config['name'], self.start_time)
+        self.writer = WriterTensorboardX(writer_dir, logger, global_config['visualization']['tensorboardX'])
         self.start_epoch = 1
         self.train_iteration_count = 0
         self.valid_iteration_counts = [0] * len(self.valid_data_loaders)
@@ -155,7 +154,7 @@ class BasePipeline(ABC):
         self.valid_iteration_counts = list(self.valid_iteration_counts)
 
         # load architecture params from checkpoint.
-        if checkpoint['config']['arch'] != self.config['arch']:
+        if checkpoint['config']['arch'] != global_config['arch']:
             logger.warning(
                 'Warning: Architecture configuration given in config file is different from that of checkpoint. '
                 'This may yield an exception while state_dict is being loaded.'
@@ -164,7 +163,7 @@ class BasePipeline(ABC):
         model.load_state_dict(checkpoint['state_dict'])
 
         # load optimizer state from checkpoint only when optimizer type is not changed.
-        if checkpoint['config']['optimizer']['type'] != self.config['optimizer']['type']:
+        if checkpoint['config']['optimizer']['type'] != global_config['optimizer']['type']:
             logger.warning('Warning: Optimizer type given in config file is different from that of checkpoint. '
                            'Optimizer parameters not being resumed.')
         elif self.optimizer is None:
@@ -178,7 +177,7 @@ class BasePipeline(ABC):
         # print logged informations to the screen
         self.writer.set_step(epoch, 'epoch_average')
         for key, value in log.items():
-            if self.verbosity >= 1:
+            if global_config['trainer']['verbosity'] >= 1:
                 logger.info('    {:20s}: {:.4f}'.format(str(key), value))
             if 'epoch' not in key:
                 self.writer.add_scalar(key, value)
@@ -223,7 +222,7 @@ class BasePipeline(ABC):
             'state_dict': model_state,
             'optimizer': self.optimizer.state_dict(),
             'monitor_best': self.monitor_best,
-            'config': self.config,
+            'config': global_config,
             'train_iteration_count': self.train_iteration_count,
             'valid_iteration_counts': self.valid_iteration_counts,
         }
