8475062ea26474ac9ecf64def5061aec80fb0009
YaLiangChang <b03901014@ntu.edu.tw>
2019-07-22 18:24:57 +0800

    Enhance readability of global_config.py: add comments and rename functions

--- src/utils/global_config.py
+++ src/utils/global_config.py
@@ -1,3 +1,9 @@
+'''
+global_config.py
+
+Global configuration module with a global var "global_config" for other modules to access
+all configuration.
+'''
 from copy import deepcopy
 
 import json
@@ -6,18 +12,30 @@ from attrdict import AttrDict
 from .logging_config import logger
 
 
-def iter_attr_dict(attr_dict: AttrDict, root_path: str, key_value_pair_dict: AttrDict):
-    for k, v in attr_dict.items():
+def flatten_nested_dict(nested_dict: dict, root_path: str, flatten_dict: dict = {}):
+    """ Recursively iterate all values in a nested dictionary and return a flatten one.
+
+    Args:
+        nested_dict (dict): the nested dictionary to be flatten
+        root_path (str): node path, viewing nested_dict as a tree
+        flatten_dict (dict): recorded flatten_dict for recursive call
+
+    Returns:
+        flatten_dict (dict): flatten dictionary with flatten_key (root_node/leavenode/...) as path
+
+    """
+    for k, v in nested_dict.items():
         current_path = f"{root_path}/{k}" if root_path != "" else k
 
         if type(v) != dict:
-            key_value_pair_dict[current_path] = v
+            flatten_dict[current_path] = v
         else:
-            key_value_pair_dict = iter_attr_dict(v, current_path, key_value_pair_dict)
-    return key_value_pair_dict
+            flatten_dict = flatten_nested_dict(v, current_path, flatten_dict)
+    return flatten_dict
 
 
 def get_value_in_nested_dict(nested_dict: dict, keys: list):
+    """ Get a value in a nested dictionary given a sequential key list. """
     temp = nested_dict
     for i, k in enumerate(keys):
         temp = temp[k]
@@ -25,17 +43,28 @@ def get_value_in_nested_dict(nested_dict: dict, keys: list):
             return temp
 
 
-def get_changed_and_added_config(template_config, specified_config):
+def get_changed_and_added_config(template_config: dict, specified_config: dict):
+    """ Compare the difference between template config and specified config,
+    and return changed (include added) and added config.
+    """
     changed_config = {}
     added_config = {}
-    flatten_template_config = iter_attr_dict(template_config, "", {})
-    flatten_specified_config = iter_attr_dict(specified_config, "", {})
+    # Flatten nested dictionaries
+    flatten_template_config = flatten_nested_dict(template_config, "")
+    flatten_specified_config = flatten_nested_dict(specified_config, "")
+
+    # Check each value in specified_config to see if it is different from the template
     for k, v in flatten_specified_config.items():
+        # Concatenate if it is name
         if k == 'name':
             changed_config['name'] = f"{template_config['name']}+{specified_config['name']}"
+
+        # Added to changed_config only if it is different from the tempalte
         elif k in flatten_template_config:
             if v != flatten_template_config[k]:
                 changed_config[k] = v
+
+        # Added to both added_config changed_config if it is new
         else:
             changed_config[k] = v
             added_config[k] = v
@@ -43,17 +72,21 @@ def get_changed_and_added_config(template_config, specified_config):
 
 
 def merge_template_and_changed_config(template_config, changed_config):
+    """ Merge the template and changed config as a global_config. """
     merged_config = deepcopy(template_config)
     for k, v in changed_config.items():
         keys = k.split('/')
-        temp = merged_config
+
+        # Trace the path by the key and current_dict
+        current_dict = merged_config
         for i, k in enumerate(keys):
             if i == len(keys) - 1:
-                temp[k] = v
+                current_dict[k] = v
             else:
-                if k not in temp:
-                    temp[k] = {}
-                temp = temp[k]
+                # If it is added, create a new dictionary for it
+                if k not in current_dict:
+                    current_dict[k] = {}
+                current_dict = current_dict[k]
     return merged_config
 
 
@@ -62,47 +95,57 @@ class SingleGlobalConfig(AttrDict):
 
     It needs to be setup first by main.py
 
+    It is a AttrDict with additional functions for template/specified config settings.
+
     One could use either global_config.some_attribute or global_config['some_attribute']
     to access the config
     """
-    def setup(self, template_config_filename, specified_config_filenames):
+    def setup(self, template_config_filename: list, specified_config_filenames: list):
+        """ Setup the global_config. """
+        # NOTE: this function needs to be called by main.py before imported by modules unless it is resumed
         self._load_template_conifg(template_config_filename)
         self._load_specified_configs(specified_config_filenames)
         self._get_changed_and_merged_config()
         self.set_config(self.merged_config)
 
-    def set_config(self, config):
+    def set_config(self, config: list):
+        """ Set the config. """
         for k, v in config.items():
             self[k] = v
 
     def __print__(self):
+        """ Print all key & value pairs. """
         for k, v in self.items():
             logger.info(f"{k}: {v}")
 
     def print_changed(self):
+        """ Print all changed/added values. """
         for k, v in self.changed_config.items():
             if k in self.added_config:
                 logger.info(f"Added key: {k} ({v})")
             else:
                 original_value = get_value_in_nested_dict(self.template_config, k.split('/'))
-                breakpoint()
                 logger.warning(f"Changed key: {k} ({original_value} -> {v})")
 
-    def _load_template_conifg(self, config_filename):
-        # Note that for some reason this is not mutable
+    def _load_template_conifg(self, config_filename: str):
+        """ Load the template config. """
+        # Note that for some reason this is not mutable?
         with open(config_filename) as fin:
             self.template_config = json.load(fin)
 
-    def _load_specified_configs(self, config_filenames):
-        # Note that for some reason this is not mutable
+    def _load_specified_configs(self, config_filenames: list):
+        """ Load specified config(s). """
+        # Note that for some reason this is not mutable?
         self.specified_config = self._extend_configs({}, config_filenames)
 
     def _get_changed_and_merged_config(self):
+        """ Compare specified_config and template_config to get changed_config/merged_config. """
         self.changed_config, self.added_config = \
             get_changed_and_added_config(self.template_config, self.specified_config)
         self.merged_config = merge_template_and_changed_config(self.template_config, self.changed_config)
 
-    def _extend_configs(self, config, config_filenames: list):
+    def _extend_configs(self, config: dict, config_filenames: list):
+        """ Extend a dict config with several config files. """
         # load config files, the overlapped entries will be overwriten
         for config_filename in config_filenames:
             with open(config_filename) as fin:
@@ -110,7 +153,8 @@ class SingleGlobalConfig(AttrDict):
                 config = self._extend_config(config, added_config)
         return config
 
-    def _extend_config(self, config, added_config):
+    def _extend_config(self, config: dict, added_config: dict):
+        """ Extend a dict config with an dict added_config"""
         for key, value in added_config.items():
             if key in config.keys():
                 if key == 'name':
