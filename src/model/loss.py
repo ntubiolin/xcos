93452ac205dc694dec1e68ad6d691a9e6ce5bdb5
Zhe-Yu Liu <j2325138@gmail.com>
2019-08-11 20:14:38 +0800

    Add hinge loss class and GAN module forward

--- src/model/loss.py
+++ src/model/loss.py
@@ -27,3 +27,29 @@ class CrossEntropyLoss(BaseLoss):
     def __init__(self, *args, **kargs):
         super().__init__(*args, **kargs)
         self.loss_fn = nn.CrossEntropyLoss()
+
+
+# Formulation reference: https://arxiv.org/pdf/1802.05957.pdf (eq. 17)
+class HingeLossG(nn.Module):
+    def __init__(self, nickname=None, weight=1):
+        self.weight = weight
+        self.nickname = self.__class__.__name__ if nickname is None else nickname
+
+    def forward(self, data_dict, output_dict):
+        D_G_z = output_dict['D_G_z']
+        return (-D_G_z).mean()
+
+
+# Formulation reference: https://arxiv.org/pdf/1802.05957.pdf (eq. 16)
+class HingeLossD(nn.Module):
+    def __init__(self, nickname=None, weight=1):
+        self.weight = weight
+        self.nickname = self.__class__.__name__ if nickname is None else nickname
+        self.relu = nn.ReLU()
+
+    def forward(self, data_dict, output_dict):
+        D_G_z = output_dict['D_G_z']
+        D_x = output_dict['D_x']
+        loss_real = self.relu(1 - D_x).mean()
+        loss_fake = self.relu(1 + D_G_z).mean()
+        return loss_real + loss_fake
--- src/model/model.py
+++ src/model/model.py
@@ -2,8 +2,10 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))  # noqa
 
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.utils import spectral_norm
 
 from model.base_model import BaseModel
 
@@ -81,11 +83,24 @@ class MnistDiscriminator(nn.Module):
         return x
 
 
-class MnistSNGAN(BaseModel):
-    def __init__(self):
+class MnistGAN(BaseModel):
+    def __init__(self, spectral_normalization=True):
         super.__init__()
         self.generator = MnistGenerator()
         self.discriminator = MnistDiscriminator()
+        if spectral_normalization:
+            self.discriminator = spectral_norm(self.discriminator)
 
     def forward(self, data_dict):
-        pass
+        x = data_dict['data_input']
+        batch_size = x.size(0)
+        z = torch.randn((batch_size, 100)).view(-1, 100, 1, 1).to(self.device)
+        G_z = self.generator(z)
+        D_G_z = self.discriminator(G_z)
+        D_x = self.discriminator(x)
+
+        return {
+            "G_z": G_z,
+            "D_G_z": D_G_z,
+            "D_x": D_x
+        }
