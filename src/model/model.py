a97a139c4bc798cf34208e987695076a4213d1c9
Zhe-Yu Liu <j2325138@gmail.com>
2019-08-11 15:36:52 +0800

    Add architectures of G and D

--- src/model/model.py
+++ src/model/model.py
@@ -32,3 +32,60 @@ class MnistModel(BaseModel):
         return {
             "model_output": out
         }
+
+
+class MnistGenerator(nn.Module):
+    # architecture reference: https://github.com/znxlwm/pytorch-MNIST-CelebA-GAN-DCGAN/blob/master/pytorch_MNIST_DCGAN.py  # NOQA
+    def __init__(self, d=128):
+        super().__init__()
+        self.deconv1 = nn.ConvTranspose2d(100, d * 8, 4, 1, 0)
+        self.deconv1_bn = nn.BatchNorm2d(d * 8)
+        self.deconv2 = nn.ConvTranspose2d(d * 8, d * 4, 4, 2, 1)
+        self.deconv2_bn = nn.BatchNorm2d(d * 4)
+        self.deconv3 = nn.ConvTranspose2d(d * 4, d * 2, 4, 2, 1)
+        self.deconv3_bn = nn.BatchNorm2d(d * 2)
+        self.deconv4 = nn.ConvTranspose2d(d * 2, d, 4, 2, 1)
+        self.deconv4_bn = nn.BatchNorm2d(d)
+        self.deconv5 = nn.ConvTranspose2d(d, 1, 4, 2, 1)
+
+    def forward(self, input):
+        # x = F.relu(self.deconv1(input))
+        x = F.relu(self.deconv1_bn(self.deconv1(input)))
+        x = F.relu(self.deconv2_bn(self.deconv2(x)))
+        x = F.relu(self.deconv3_bn(self.deconv3(x)))
+        x = F.relu(self.deconv4_bn(self.deconv4(x)))
+        x = F.tanh(self.deconv5(x))
+        return x
+
+
+class MnistDiscriminator(nn.Module):
+    # architecture reference: https://github.com/znxlwm/pytorch-MNIST-CelebA-GAN-DCGAN/blob/master/pytorch_MNIST_DCGAN.py  # NOQA
+    def __init__(self, d=128):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, d, 4, 2, 1)
+        self.conv2 = nn.Conv2d(d, d * 2, 4, 2, 1)
+        self.conv2_bn = nn.BatchNorm2d(d * 2)
+        self.conv3 = nn.Conv2d(d * 2, d * 4, 4, 2, 1)
+        self.conv3_bn = nn.BatchNorm2d(d * 4)
+        self.conv4 = nn.Conv2d(d * 4, d * 8, 4, 2, 1)
+        self.conv4_bn = nn.BatchNorm2d(d * 8)
+        self.conv5 = nn.Conv2d(d * 8, 1, 4, 1, 0)
+
+    # forward method
+    def forward(self, input):
+        x = F.leaky_relu(self.conv1(input), 0.2)
+        x = F.leaky_relu(self.conv2_bn(self.conv2(x)), 0.2)
+        x = F.leaky_relu(self.conv3_bn(self.conv3(x)), 0.2)
+        x = F.leaky_relu(self.conv4_bn(self.conv4(x)), 0.2)
+        x = F.sigmoid(self.conv5(x))
+        return x
+
+
+class MnistSNGAN(BaseModel):
+    def __init__(self):
+        super.__init__()
+        self.generator = MnistGenerator()
+        self.discriminator = MnistDiscriminator()
+
+    def forward(self, data_dict):
+        pass
