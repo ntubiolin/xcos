b81cfc59475bb9ddebf65255ac1d6ddd40ddc89e
ZheYu <j2325138@gmail.com>
2019-08-15 15:49:28 +0800

    Create offline version of FIDScore

--- src/model/metric.py
+++ src/model/metric.py
@@ -2,6 +2,9 @@ import os
 import torch
 from abc import abstractmethod
 import importlib.util
+import tempfile
+
+from torchvision import transforms
 
 from utils.util import UnNormalize, lib_path
 
@@ -53,26 +56,48 @@ class TopKAcc(BaseMetric):
         return self.total_correct / self.total_number
 
 
-class FIDScore(torch.nn.Module):
-    spec = importlib.util.spec_from_file_location("fid_score", os.path.join(lib_path, 'pytorch_fid/fid_score.py'))
-    fid_score = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(fid_score)
+class FIDScoreOffline(torch.nn.Module):
+    """
+    Module calculating FID score
+    """
 
     def __init__(self, output_key, target_key, unnorm_mean=(0.5,), unnorm_std=(0.5,), nickname="FID_InceptionV3"):
         super().__init__()
         self.nickname = nickname
         self.output_key = output_key
         self.target_key = target_key
-        self.unnormalizer = UnNormalize(unnorm_mean, unnorm_mean)
+        self.from_tensor_to_pil = transforms.Compose([
+            UnNormalize(unnorm_mean, unnorm_mean),
+            transforms.ToPILImage()
+        ])
+        self.tmp_gt_dir = tempfile.TemporaryDirectory(prefix='gt_')
+        self.tmp_out_dir = tempfile.TemporaryDirectory(prefix='out_')
+
+        # Load module fid_score given path
+        spec = importlib.util.spec_from_file_location("fid_score", os.path.join(lib_path, 'pytorch_fid/fid_score.py'))
+        self.fid_score = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(self.fid_score)
 
     def clear(self):
-        # TODO: create containers of temporary files using TemporaryDirectory, etc. in package tempfile
-        pass
+        self.tmp_gt_dir.cleanup()
+        self.tmp_out_dir.cleanup()
+        self.tmp_gt_dir = tempfile.TemporaryDirectory(prefix='gt_')
+        self.tmp_out_dir = tempfile.TemporaryDirectory(prefix='out_')
+
+    def _save_img_tensor(self, tensor, buffer_dir):
+        """ Save image tensor to a named temporary file and return the name."""
+        temp_f = tempfile.NamedTemporaryFile(suffix='.png', dir=buffer_dir.name, delete=False)
+        pil_image = self.from_tensor_to_pil(tensor.cpu())
+        pil_image.save(temp_f)
+        temp_f.close()
 
     def update(self, data, output):
-        # TODO: unnormalize real and fake images, save them to temporary files.
+        for gt_tensor, out_tensor in zip(data[self.target_key], output[self.output_key]):
+            self._save_img_tensor(gt_tensor, self.tmp_gt_dir)
+            self._save_img_tensor(out_tensor.clamp(-1, 1), self.tmp_out_dir)
         return None
 
     def finalize(self):
         # TODO: calculate fid scores using functions in libs/pytorch_fid/fid_score.py
+        self.fid_score.calculate_fid_given_paths()
         return 0
